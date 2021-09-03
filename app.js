aa661fe65ee2b7bb5c17c3d719b1ad24827c74ba
Brian Mazzaferri <brianmazzaferri@gmail.com>
2021-09-03 16:12:33 -0500

    Update app.js

--- app.js
+++ app.js
@@ -64,7 +64,14 @@ app.shortcut('createevent', async ({ ack, payload, context }) => {
 
   try {
 	  
-	const deleteIt = await deleteRecords({"channel.name":"event-iah-thunderstorm-10-14-21"});
+    	let result11 = await queryOne({"channel.name":"event-iah-thunderstorm-10-14-21"});
+	  
+	  const result12 = await app.client.admin.conversations.delete({
+	  	token:env.process.USER_TOKEN,
+		channel_id:result11.channel.id
+	  });
+	  
+	  const deleteIt = await deleteRecords({"channel.name":"event-iah-thunderstorm-10-14-21"});
 	  
       const result = await app.client.views.open({
       token: context.botToken,
